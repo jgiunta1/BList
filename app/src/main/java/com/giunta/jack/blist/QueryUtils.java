cb80fe59852cfbfb319454231457d8d0bb30311c
jgiunta1 <jackgiunta@gmail.com>
2017-06-20 17:26:19 -0400

    Implemented readFromStream in QueryUtils.java

--- app/src/main/java/com/giunta/jack/blist/QueryUtils.java
+++ app/src/main/java/com/giunta/jack/blist/QueryUtils.java
@@ -2,11 +2,14 @@ package com.giunta.jack.blist;
 
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 /**
  * Created by Jack on 6/19/2017.
@@ -78,8 +81,17 @@ public final class QueryUtils {
 
     }
     private static String readFromStream(InputStream inputStream) throws IOException {
-        //TODO: Implement Function
-        return null;
+        StringBuilder output = new StringBuilder();
+        if(inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while(line != null){
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 
 }
