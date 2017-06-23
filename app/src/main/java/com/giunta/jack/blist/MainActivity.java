213eab9590e465d9c14cb521f6e7cd90215bf121
jgiunta1 <jackgiunta@gmail.com>
2017-06-22 21:30:50 -0400

    Implemented LoaderCallbacks in MainActivity

--- app/src/main/java/com/giunta/jack/blist/MainActivity.java
+++ app/src/main/java/com/giunta/jack/blist/MainActivity.java
@@ -1,20 +1,26 @@
 package com.giunta.jack.blist;
 
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<Book>> {
+    public static final String LOG_TAG = "MAIN";
+    private static final int BOOK_LOADER_ID = 1;
     private static EditText keywordView;
     private BookAdapter bookAdapter;
+    private static String keyword;
     private TextView emptyView;
 
     @Override
@@ -39,10 +45,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void searchBooks(View v){
-        String keyword = keywordView.getText().toString();
+        keyword = keywordView.getText().toString();
+
+        if(TextUtils.isEmpty(keyword)){
+            Toast.makeText(MainActivity.this, "INVALID KEYWORD", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // TODO Show loading bar
+
+
+        getLoaderManager().initLoader(BOOK_LOADER_ID, null, this);
+
 
         // Temporarily Load Sample Data into ListView
-        bookAdapter.clear();
+/*        bookAdapter.clear();
         List<String> authors = new ArrayList<>();
         authors.add("Author1");
         authors.add("Author2");
@@ -56,6 +73,29 @@ public class MainActivity extends AppCompatActivity {
         data.add(b2);
         data.add(b3);
 
-        bookAdapter.addAll(data);
+        bookAdapter.addAll(data);*/
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
+        return new BookLoader(this, keyword);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
+        emptyView.setText("No Results");
+
+        bookAdapter.clear();
+
+        if(books != null && !books.isEmpty()){
+            bookAdapter.addAll(books);
+        } else {
+            Log.e(LOG_TAG, "Error Populating List");
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        bookAdapter.clear();
     }
 }
